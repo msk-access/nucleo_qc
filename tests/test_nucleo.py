4f4eb05fc6f82606f72cdb1e3d5e5ea666cb0e64
EricWilliam Buehler <buehlere@selene.cbio.private>
2022-09-20 14:37:30 -0400

    adding aggregate

--- tests/test_nucleo.py
+++ tests/test_nucleo.py
@@ -24,51 +24,29 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 coloredlogs.install(level="DEBUG")
 
-RESULT_FILE_NAME = [
-    "test_collapsed_FM.bai",
-    "test_collapsed_FM.bam",
-    "test_collapsed_aln_metrics.txt",
-    "test_collapsed_duplex.bai",
-    "test_collapsed_duplex.bam",
-    "test_collapsed_duplex_aln_metrics.txt",
-    "test_collapsed_grouped.bam",
-    "test_collapsed_simplex.bai",
-    "test_collapsed_simplex.bam",
-    "test_collapsed_simplex_aln_metrics.txt",
-    "test_fastp_report.html",
-    "test_fastp_report.json",
-    "test_umi_family_size.hist",
-    "test_uncollapsed_BR.bai",
-    "test_uncollapsed_BR.bam",
-    "test_uncollapsed_BR_alignment_summary_metrics.txt",
-    "test_uncollapsed_FM.bai",
-    "test_uncollapsed_FM.bam",
-    "test_uncollapsed_MD_metrics.txt",
-    "pipeline_result.json"
-]
+OUTPUT_JSON_SINGLE_FILENAME = "pipeline_result_single.json"
+OUTPUT_JSON_AGGREGATE_FILENAME = "pipeline_result_aggregate.json"
 
-OUTPUT_JSON_FILENAME = "pipeline_result.json"
-EXPECTED_OUTPUT_JSON_FILENAME = "expected_output.json"
 
-def test_setup_module(platform):
+def test_single_sample(platform):
     """
     Setup and Test the workflow with cwltool
     """
     
     logging.info("### SETUP ###")
-    with open(OUTPUT_JSON_FILENAME, "w") as json:
+    with open(OUTPUT_JSON_SINGLE_FILENAME, "w") as json:
         if platform == "singularity":
             cmd = [
                 "cwltool",
                 "--singularity",
                 "nucleo_qc.cwl",
-                "nucleo_qc_myeloid_2.yml",
+                "tests/inputs/nucleo_qc_myeloid_2.yml",
             ]
         elif platform == "docker": 
             cmd = [
                 "cwltool",
                 "nucleo_qc.cwl",
-                "nucleo_qc_myeloid_2.yml",
+                "tests/inputs/nucleo_qc_myeloid_2.yml",
             ]
         else: 
             assert platform in ['docker', 'singularity'] 
@@ -76,17 +54,64 @@ def test_setup_module(platform):
         process = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=json, close_fds=True)
         ret_code = process.wait()
         assert process.returncode == 0, process.stderr
-        return(ret_code)
+        
 
 
-def test_output_json():
+def test_single_sample_output():
 
 #     """
 #     General tests for output json
 #     """
     logging.info(
-        "### Check if json file exists and check some basic stats ###")
-    assert os.path.exists(OUTPUT_JSON_FILENAME)
-    output_json = json.loads(open(OUTPUT_JSON_FILENAME, 'r').read())
+    "### Check if json file exists and check some basic stats ###")
+    assert os.path.exists(OUTPUT_JSON_SINGLE_FILENAME)
+    output_json = json.loads(open(OUTPUT_JSON_SINGLE_FILENAME, 'r').read())
     assert (output_json["multiqc_html"]["basename"] == "multiqc_1.10.1.7.html")
+    assert (os.path.isfile('multiqc_1.10.1.7/multiqc_1.10.1.7.html') )
+    shutil.rmtree('multiqc_1.10.1.7')
+    shutil.rmtree('Myeloid200-2-05500HJ_P20')
 
+
+
+def test_aggregate_visual(platform):
+    """
+    Setup and Test the workflow with cwltool
+    """
+    
+    logging.info("### SETUP ###")
+    with open(OUTPUT_JSON_AGGREGATE_FILENAME, "w") as json:
+        if platform == "singularity":
+            cmd = [
+                "cwltool",
+                "nucleo_aggregate_visualize.cwl",
+                "tests/inputs/nucleo_agg_vis_2.yaml",
+            ]
+        elif platform == "docker": 
+            cmd = [
+                "cwltool",
+                "--singularity",
+                "nucleo_aggregate_visualize.cwl",
+                "tests/inputs/nucleo_agg_vis_2.yaml",
+            ]
+        else: 
+            assert platform in ['docker', 'singularity'] 
+
+        logging.info("setup_module: cmd being executed, %s", " ".join(cmd))
+        process = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=json, close_fds=True)
+        ret_code = process.wait()
+        assert process.returncode == 0, process.stderr
+        
+
+
+def test_aggregate_visual_output():
+
+#     """
+#     General tests for output json
+#     """
+    logging.info(
+    "### Check if json file exists and check some basic stats ###")
+    assert os.path.exists(OUTPUT_JSON_AGGREGATE_FILENAME )
+    output_json = json.loads(open(OUTPUT_JSON_AGGREGATE_FILENAME, 'r').read())
+    assert (output_json["multiqc_html"]["basename"] == "multiqc_1.10.1.7.html")
+    assert (os.path.isfile('multiqc_1.10.1.7/multiqc_1.10.1.7.html') )
+    shutil.rmtree('multiqc_1.10.1.7')
