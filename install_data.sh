167409d66911c995c53598764a3d34ff1edfda61
buehlere <buehlere77@gmail.com>
2022-11-28 10:10:38 -0500

    update gdrive download
    :heavy_check_mark: fixing data download, no longer need to use gdown, found url that works with wget
    :heavy_check_mark: remove gdown req

--- install_data.sh
+++ install_data.sh
@@ -2,19 +2,17 @@
 
 
 # Test data is hosted on Google Drive at:
-# https://drive.google.com/file/d/1i1GA4AZhuw6Si8RmPFPJxmejIUt3ZZ1D/view?usp=sharing
-
-fileid=1i1GA4AZhuw6Si8RmPFPJxmejIUt3ZZ1D
+# https://drive.google.com/u/0/uc?id=1i1GA4AZhuw6Si8RmPFPJxmejIUt3ZZ1D
 
 filename=test_nucleo_qc.tar.gz
-foldername=test_nucleo_qc
-gdown https://drive.google.com/uc?id=$fileid 
-
+# Note: Drive seems to change somewhat frequently how it handles endpoints for large files 
+# add confirm parameter to link for this to work 
+# previously used another wget scheme and gdown
+wget -O $filename  "https://drive.google.com/u/0/uc?id=1i1GA4AZhuw6Si8RmPFPJxmejIUt3ZZ1D&export=download&confirm=t"
 # Suppress linux warnings for MacOS tar.gz files
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
     tar --warning=no-unknown-keyword -xzvf $filename -C tests/inputs/
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     tar -xzvf $filename -C tests/inputs/
 fi
-
 rm $filename
