374b9cda839067395f2ec1ac4b510398b894cea2
buehlere <buehlere77@gmail.com>
2022-12-01 10:50:04 -0500

    re-org with data fix
    :heavy_check_mark: re-add tests now that data is fixed.
    :heavy_check_mark: keep wget change
    :heavy_check_mark: keep tox change
    :heavy_check_mark: revert gitactions

--- tests/test_nucleo_qc.py
+++ tests/test_nucleo_qc.py
@@ -28,46 +28,48 @@ OUTPUT_JSON_SINGLE_FILENAME = "pipeline_result_single.json"
 OUTPUT_JSON_AGGREGATE_FILENAME = "pipeline_result_aggregate.json"
 
 
-# def test_single_sample(platform):
-#     """
-#     Test Single Sample Workflow workflow with cwltool
-#     """
+def test_single_sample(platform):
+    """
+    Test Single Sample Workflow workflow with cwltool
+    """
     
-#     logging.info("### SETUP ###")
-#     with open(OUTPUT_JSON_SINGLE_FILENAME, "w") as json:
-#         if platform == "singularity":
-#             cmd = [
-#                 "cwltool",
-#                 "--singularity",
-#                 "nucleo_qc.cwl",
-#                 "tests/inputs/nucleo_qc_myeloid_2.yml",
-#             ]
-#         elif platform == "docker": 
-#             cmd = [
-#                 "cwltool",
-#                 "nucleo_qc.cwl",
-#                 "tests/inputs/nucleo_qc_myeloid_2.yml",
-#             ]
-#         else: 
-#             assert platform in ['docker', 'singularity'] 
-#         logging.info("setup_module: cmd being executed, %s", " ".join(cmd))
-#         process = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=json, close_fds=True)
-#         ret_code = process.wait()
-#         assert process.returncode == 0, process.stderr
+    logging.info("### SETUP ###")
+    with open(OUTPUT_JSON_SINGLE_FILENAME, "w") as json:
+        if platform == "singularity":
+            cmd = [
+                "cwltool",
+                "--singularity",
+                "nucleo_qc.cwl",
+                "tests/inputs/nucleo_qc_myeloid_2.yml",
+            ]
+        elif platform == "docker": 
+            cmd = [
+                "cwltool",
+                "nucleo_qc.cwl",
+                "tests/inputs/nucleo_qc_myeloid_2.yml",
+            ]
+        else: 
+            assert platform in ['docker', 'singularity'] 
+        logging.info("setup_module: cmd being executed, %s", " ".join(cmd))
+        process = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=json, close_fds=True)
+        ret_code = process.wait()
+        assert process.returncode == 0, process.stderr
         
 
 
-# def test_single_sample_output():
+def test_single_sample_output():
 
-#     """
-#     Test Output of Single Sample Workflow
-#     """
-#     logging.info(
-#     "### Check if json file exists and check some basic stats ###")
-#     assert os.path.exists(OUTPUT_JSON_SINGLE_FILENAME)
-#     assert (os.path.isfile('multiqc_1.10.1.7/multiqc_1.10.1.7.html') )
-#     shutil.rmtree('multiqc_1.10.1.7')
-#     shutil.rmtree('Myeloid200-2-05500HJ_P20')
+    """
+    Test Output of Single Sample Workflow
+    """
+    logging.info(
+    "### Check if json file exists and check some basic stats ###")
+    assert os.path.exists(OUTPUT_JSON_SINGLE_FILENAME)
+    output_json = json.loads(open(OUTPUT_JSON_SINGLE_FILENAME, 'r').read())
+    assert (output_json["multiqc_html"]["basename"] == "multiqc_1.10.1.7.html")
+    assert (os.path.isfile('multiqc_1.10.1.7/multiqc_1.10.1.7.html') )
+    shutil.rmtree('multiqc_1.10.1.7')
+    shutil.rmtree('Myeloid200-2-05500HJ_P20')
 
 
 
@@ -77,26 +79,27 @@ def test_aggregate_visual(platform):
     """
     
     logging.info("### SETUP ###")
-    if platform == "singularity":
-        cmd = [
-            "cwltool",
-            "--singularity",
-            "nucleo_aggregate_visualize.cwl",
-            "tests/inputs/nucleo_agg_vis_2.yaml",
-        ]
-    elif platform == "docker": 
-        cmd = [
-            "cwltool",
-            "nucleo_aggregate_visualize.cwl",
-            "tests/inputs/nucleo_agg_vis_2.yaml",
-        ]
-    else: 
-        assert platform in ['docker', 'singularity'] 
+    with open(OUTPUT_JSON_AGGREGATE_FILENAME, "w") as json:
+        if platform == "singularity":
+            cmd = [
+                "cwltool",
+                "--singularity",
+                "nucleo_aggregate_visualize.cwl",
+                "tests/inputs/nucleo_agg_vis_2.yaml",
+            ]
+        elif platform == "docker": 
+            cmd = [
+                "cwltool",
+                "nucleo_aggregate_visualize.cwl",
+                "tests/inputs/nucleo_agg_vis_2.yaml",
+            ]
+        else: 
+            assert platform in ['docker', 'singularity'] 
 
-    logging.info("setup_module: cmd being executed, %s", " ".join(cmd))
-    process = subprocess.run(cmd)
-    #ret_code = process.wait()
-    assert process.returncode == 0, process.stderr
+        logging.info("setup_module: cmd being executed, %s", " ".join(cmd))
+        process = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=json, close_fds=True)
+        ret_code = process.wait()
+        assert process.returncode == 0, process.stderr
         
 
 
@@ -107,5 +110,8 @@ def test_aggregate_visual_output():
     """
     logging.info(
     "### Check if json file exists and check some basic stats ###")
+    assert os.path.exists(OUTPUT_JSON_AGGREGATE_FILENAME )
+    output_json = json.loads(open(OUTPUT_JSON_AGGREGATE_FILENAME, 'r').read())
+    assert (output_json["multiqc_html"]["basename"] == "multiqc_1.10.1.7.html")
     assert (os.path.isfile('multiqc_1.10.1.7/multiqc_1.10.1.7.html') )
     shutil.rmtree('multiqc_1.10.1.7')
