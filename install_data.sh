288e93a661b730741e2d6bae491360059f740935
Ronak Shah <shahr2@mskcc.org>
2024-06-18 02:10:42 -0400

    Update install_data.sh

--- install_data.sh
+++ install_data.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-
 # Test data is hosted on Google Drive at:
 # https://drive.google.com/u/0/uc?id=1i1GA4AZhuw6Si8RmPFPJxmejIUt3ZZ1D
 
 fileid=1i1GA4AZhuw6Si8RmPFPJxmejIUt3ZZ1D
 
 filename=test_nucleo_qc.tar.gz
-foldername=test_nucleo_qc
+foldername=test_data
 
 # Skip if already have test data
 [[ -f $filename ]] && exit 0
@@ -15,7 +14,7 @@ foldername=test_nucleo_qc
 
 #curl -c ./cookie -s -k -L "https://drive.google.com/uc?export=download&id=$fileid" > /dev/null
 #curl -k -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
-
+cd tests/inputs || exit
 curl -L "https://drive.usercontent.google.com/download?id=${fileid}&confirm=xxx" -o ${filename}
 
 # Suppress linux warnings for MacOS tar.gz files
